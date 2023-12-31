dceadb034fb8325e37b4cd728b6f757cb8ae0c1a
Adrian John <adrianjohn.developer@gmail.com>
2023-12-31 16:47:45 -0400

    setup protected routes

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require("./routes/authRoutes");
 const keys = require("./config/keys.js")
-const passport = require('passport');
+const cookieParser  = require('cookie-parser');
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 // app.use(passport.initialize());
 // require('./server/config/passport.js')(passport);
@@ -20,3 +21,4 @@ mongoose.connect(keys["mongoURI"], { useNewUrlParser: true, useUnifiedTopology: 
 // routes
 app.get('/', (req, res) => res.send('Hello, World!')); 
 app.use(authRoutes);
+ 
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const authControler = require("../controllers/authController");
-const passport = require('passport');
+const protect = require("../middleware/authMiddleware")
 
 const router = Router(); 
 router.post("/signup" , authControler.signup_post);
-router.post("/profile" , passport.authenticate('jwt', { session: false }), authControler.update_profile_info);
+// router.post("/profile" , passport.authenticate('jwt', { session: false }), authControler.update_profile_info);
 router.post("/login" , authControler.login_post);
 router.post("/logout" , authControler.logout_post);
+
+router.get("/profile" ,protect, authControler.update_profile_info)
 module.exports = router;
