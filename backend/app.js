dd917ca8921971ebd31f9605fb8fa1a0bf558162
Adrian John <adrianjohn.developer@gmail.com>
2024-07-03 22:14:10 -0500

    setup ws with express

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,47 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require("./routes/authRoutes");
+const chatRoutes = require('./routes/chatRoutes')
 const keys = require("./config/keys.js")
-const cookieParser  = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const enableWs = require('express-ws')
+
 
 const app = express();
+enableWs(app);
 
 app.use(express.json());
 app.use(cookieParser());
- 
+
 const allowed_origins = ["http://localhost:3000"];
 
 // database connection 
-mongoose.connect(keys["mongoURI"], { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(4000))
+mongoose.connect(keys["mongoURI"])
+  .then(result => {
+    app.ws('/echo', (ws, req) => {
+      ws.on('message', msg => {
+        ws.send(msg)
+      })
+
+      ws.on('close', () => {
+        console.log('WebSocket was closed')
+      })
+    })
+  })
+  .then((result) => {
+    app.listen(4000)
+  })
   .catch((err) => console.log(err));
 
-  app.use(cors({
-    origin: allowed_origins,
-  }));
+app.use(cors({
+  origin: allowed_origins,
+}));
+
+
 
 // routes
-app.get('/', (req, res) => res.send('Hello, World!')); 
+app.get('/', (req, res) => res.send('Hello, World!'));
 app.use(authRoutes);
+app.use(chatRoutes);
+
