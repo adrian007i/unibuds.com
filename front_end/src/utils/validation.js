afe0255c5170a43bea85c93886ecb8d1c17a4a75
Adrian John <adrianjohn.developer@gmail.com>
2024-07-24 13:13:55 -0500

    setup validation function

--- front_end/src/utils/validation.js
+++ front_end/src/utils/validation.js
@@ -1,20 +1,65 @@
-export const isEmpty = (value) => {
-  return (
-    value === undefined ||
-    value === null ||
-    (typeof value === 'object' && Object.keys(value).length === 0) ||
-    (typeof value === 'string' && value.trim().length === 0)
-  );
-};
+class Validate {
 
-export const minLength = (value, len) => {
-  return typeof value === 'string' && value.trim().length <= len
-};
+  constructor(data) {
+    const errors = {};
+    let isValid = true;
+
+    // iterate all fields eg: name, email, password
+    for (var field in data) { 
+      
+      const value = data[field][0];
+      const validateAgainst = data[field][1];
+      const min_length = data[field][2];
+
+      // iterate what we want to validate against
+      for (let i = 0; i < validateAgainst.length; i++) {
+        
+        // dynamically run the validation function
+        const test = this[validateAgainst[i]](value, min_length);
+        
+        // if we get an error, store it in the errors hashmap
+        if (test.err){
+          errors[field] = test.msg;
+          isValid = false;
+          break;
+        } 
+      } 
+    }
+    
+    return {isValid, errors}
+  }
 
 
-export const isValidEmail = (email) => { 
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email);
-};
+  isEmpty = (value) => {
+    return (
+      {
+        err: (value === undefined || value === null || (typeof value === 'object' && Object.keys(value).length === 0) || (typeof value === 'string' && value.trim().length === 0)),
+        msg: "Required"
+      }
+    );
+  };
 
+  minLength = (value, len) => {
+    return (
+      {
+        err: typeof value === 'string' && value.trim().length <= len,
+        msg: "Too Short"
+      }
+    );
+  };
+
+  isValidEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return (
+      {
+        err: !emailRegex.test(email),
+        msg: "Invalid Email"
+      }
+    );
+  };
+
+}
+
+
+export default Validate;
 
