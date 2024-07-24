f2a2ef593432528d8efd2c790e82d7a7de83bb1d
Adrian John <adrianjohn.developer@gmail.com>
2024-07-24 17:22:57 -0500

    remove old unibuds frontend

--- frontend/src/redux/constants/authConstants.js
+++
@@ -1,8 +0,0 @@
-export const REGISTER_PENDING = "REGISTER_PENDING";  
-export const REGISTER_FAIL = "REGISTER_FAIL";
-
-export const LOGIN_PENDING = "LOGIN_PENDING";
-export const LOGIN_FAIL = "LOGIN_FAIL";
-
-export const SET_USER = "SET_USER";
-export const LOGOUT_USER = "LOGOUT_USER";
