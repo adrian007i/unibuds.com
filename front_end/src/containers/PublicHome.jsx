82a76ed39d19692a781b546716cf4e682c888994
Adrian John <adrianjohn.developer@gmail.com>
2024-08-15 19:15:02 -0500

    home page clean up

--- front_end/src/containers/PublicHome.jsx
+++ front_end/src/containers/PublicHome.jsx
@@ -1,22 +1,25 @@
 import {
-    Button, 
+    Button,
     Row, Col
 } from 'react-bootstrap';
 
+import buddy from '../icons/buddy.png';
 import { Link } from 'react-router-dom';
 
 function Home() {
     return (
         <div id='home'>
 
-            <div className='top'>  
-                <p className='welcome'>Welcome to UniBuds, the only free platform to connect with students at your university</p>
-                <p>
-                    
-                    ✅ &nbsp; No Dating! Just Friends <br />
-                    ✅ &nbsp; No Download <br />
-                    ✅ &nbsp; Completely Free
-                </p>
+            <div className='top'>
+                <p className='welcome text-center'>Welcome to UniBuds, the only free platform to connect with students at your university</p>
+
+                <div className='text-center'>
+                    <img src={buddy} alt="" className='findABuddyIcon' />
+                </div>
+                
+                <p className='text-center'>No Download Necessary</p><hr /> 
+                <p className='text-center'>No Dating! Just Friends</p> <hr /> 
+                <p className='text-center'>Yup! It's completely Free</p> 
             </div>
 
             <div className='bottom'>
