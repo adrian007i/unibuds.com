83fde5ef45e4f134d3a6efcfcec5f1b514d0a59a
Adrian John <adrianjohn.developer@gmail.com>
2024-10-30 21:19:15 -0500

    fixed bug i nmain

--- front_end/src/main.jsx
+++ front_end/src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider, useSelector } from 'react-redux';
 import { store } from './state/store';
@@ -10,43 +10,54 @@ import App from './App';
 // TODO MIGHT CONSIDER REMOVING BOOTSTRAP
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import { setCurrentUser } from './containers/Auth/slice';
+import { setCurrentUser, setTokenDataPending } from './containers/Auth/slice';
 import { getChats } from './containers/Chat/slice';
 import { getUserData } from './containers/User/slice';
 import ReconnectingWebSocket from 'reconnecting-websocket';
 
 let BASE_BACKEND_URL;
 let HTTPS;
-let WSS; 
+let WSS;
 
-if (import.meta.env.MODE === 'development'){
+if (import.meta.env.MODE === 'development') {
+  // BASE_BACKEND_URL = '192.168.4.46:4000/';
   BASE_BACKEND_URL = 'localhost:4000/';
+
   HTTPS = 'http://';
   WSS = 'ws://';
 }
-else{
+else {
   BASE_BACKEND_URL = 'unibuds-com.onrender.com/';
   HTTPS = 'https://';
   WSS = 'wss://';
 }
-  
+
 
 axios.defaults.baseURL = HTTPS + BASE_BACKEND_URL;
 
 let ws = null;
 
 const Main = () => {
-  const dispatch = useDispatch();
-  dispatch(setCurrentUser(localStorage.getItem('jwtToken')));
-  
-  const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
-  if (isAuthenticated) {
-    dispatch(getChats())
-    dispatch(getUserData())
-    
-    ws = new ReconnectingWebSocket(WSS + BASE_BACKEND_URL + 'web_socket_endpoint/' + '?auth=' + localStorage.getItem('jwtToken'));
-  }
 
+  const dispatch = useDispatch();
+  const { isAuthenticated, tokenData, tokenDataPending } = useSelector(state => state.auth)
+
+
+  useEffect(() => {
+
+    if (!isAuthenticated && !tokenDataPending) {
+      dispatch(setTokenDataPending());
+      dispatch(setCurrentUser(localStorage.getItem('jwtToken')));
+    }
+
+    if (isAuthenticated){
+        dispatch(getChats(tokenData._id));
+        dispatch(getUserData());
+        ws = new ReconnectingWebSocket(WSS + BASE_BACKEND_URL + 'web_socket_endpoint/' + '?auth=' + localStorage.getItem('jwtToken'));
+    }
+
+  }, [isAuthenticated]);
+ 
 
   return <App ws={ws} />
 }
